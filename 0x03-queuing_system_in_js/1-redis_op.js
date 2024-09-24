428fe7ca94774a17f138329d52b41b101ef279d5
Kassim-gif <shuaibkassim44@gmail.com>
2024-09-24 06:38:39 +0000

    System in js

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,26 @@
+#!/usr/bin/yarn dev
+import { createClient, print } from 'redis';
+
+const client = createClient();
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to tha server:', err.toString());
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to tha server');
+});
+
+const setNewSchool = (schoolName, value) => {
+  client.SET(schoolName, value, print);
+};
+
+const displaySchoolValue = (schoolName) => {
+  client.GET(schoolName, (_err, reply) => {
+    console.log(reply);
+  });
+};
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
